69ddb05ed832d508717e59171b538ec8c7e47b3d
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-12-03 10:58:36 -0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,84 @@
+# FUNCTIONS:
+# After completing each question, call the function and ensure that it works as expected.
+
+print("Question 1:")
+# QUESTION 1. Write a function called hello that prints Hello World! 
+
+print()
+print("Question 2:")
+# QUESTION 2. Write a function called addition that accepts two parameters num1, and, num2. Print the multiplication of the two numbers. 
+
+print()
+print("Question 3:")
+# QUESTION 3. Write a function called MinutesToSeconds that converts minutes into seconds. The parameter is an integer called minutes. Print the seconds.
+
+print()
+print("Question 4:")
+# QUESTION 4. In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+# chickens = 2 legs
+# cows = 4 legs
+# pigs = 4 legs
+# The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that prints the total number of legs of all the animals.
+# Examples:
+# animals(2, 3, 5) ➞ 36
+# animals(1, 2, 3) ➞ 22
+# animals(5, 2, 8) ➞ 50
+
+print()
+print("Question 5:")
+# QUESTION 5. Create a function that takes two arguments. Both arguments are integers, a and b. Print true if one of them is 10 or if their sum is 10.
+# Examples:
+# makesTen(9, 10) ➞ true
+# makesTen(9, 9) ➞ false
+# makesTen(1, 9) ➞ true
+
+print()
+print("Question 6:")
+# QUESTION 6. Create a function that takes a number as an argument and prints negative of that number. Print negative numbers without any change.
+
+print()
+print("Question 7:")
+# QUESTION 7. Create a function that prints True if a string is empty and False otherwise.
+#Examples:
+#isEmpty("") ➞ true
+#isEmpty(" ") ➞ false
+#isEmpty("a") ➞ false
+
+print()
+print("Question 8:")
+# QUESTION 8. Write a function that checks whether a person can watch an MA15+ rated movie. One of the following two conditions is required for admittance:
+#The person is at least 15 years old.
+#They have parental supervision.
+#The function accepts two parameters, age and isSupervised. Print a boolean.
+#Examples:
+#acceptIntoMovie(14, true) ➞ true
+#acceptIntoMovie(14, false) ➞ false
+#acceptIntoMovie(16, false) ➞ true
+
+print()
+print("Question 9:")
+# QUESTION 9. Given a string, print true if its length is even or false if the length is odd.
+# Examples:
+# oddOrEven("apples") ➞ true
+# The word "apples" has 6 characters.
+# 6 is an even number, so the program outputs true.
+# oddOrEven("pears") ➞ false
+# "pears" has 5 letters, and 5 is odd.
+# Therefore the program outputs false.
+# oddOrEven("cherry") ➞ true
+
+print()
+print("Question 10:")
+# QUESTION 10. Create a function that takes a word and prints the new word without including the first character.
+# Examples:
+# newWord("apple") ➞ "pple"
+# newWord("cherry") ➞ "herry"
+# newWord("plum") ➞ "lum"
+
+print()
+print("Question 11:")
+# QUESTION 11. Create a function that takes a name and prints a greeting in the form of a string.
+#Examples:
+# helloName("Gerald") ➞ "Hello Gerald!"
+# helloName("Tiffany") ➞ "Hello Tiffany!"
+# helloName("Ed") ➞ "Hello Ed!"
